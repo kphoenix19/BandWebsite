8fe6038ca7f1540a90104daa79f50009ef7d1224
kphoenix19 <105092670+kphoenix19@users.noreply.github.com>
2026-09-04 15:38:59 -0600

    updated styling and added music page

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,9 @@
 export const band = {
   name: "Lucy's Angels",
   abbreviation: 'LA',
+  spotifyAlbumId: '1HPv7o21EOlkfxP0lY9ET3',
+  featuredTrack: {
+    name: 'Nash',
+    id: '3MZrAykeoEbAb9MKIuNMmS',
+  },
 } as const;
